170695dc1b06326e6b830919ee774a99450bf169
radman <radu.a.vlad@gmail.com>
2010-05-05 16:22:29 -0400

    added logging of errors to notification type subscriptions

--- lib/notification_system/notification_type_subscription.rb
+++ lib/notification_system/notification_type_subscription.rb
@@ -23,6 +23,7 @@ module NotificationSystem
         begin
           subscription.create_scheduled_notifications
         rescue Exception => exception
+          NotificationSystem.log("Exception of type #{exception.class} (notification_type_subscription_id = #{self.id})")
           NotificationSystem.report_exception(exception)
         end
       end
