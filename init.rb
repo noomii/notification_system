f3e8feaf77b9b2704d0de734ab647a56ce7876fe
Rafael Chacon <rafael@noomii.com>
2011-07-11 12:48:17 -0700

    Change load to autoload to avoid warning in rails 2.3

--- init.rb
+++ init.rb
@@ -4,11 +4,11 @@ notifications_path=File.join(RAILS_ROOT,'app','models','notifications')
 $: << events_path
 $: << notifications_path
 
-ActiveSupport::Dependencies.load_paths << events_path
-ActiveSupport::Dependencies.load_paths << notifications_path
+ActiveSupport::Dependencies.autoload_paths << events_path
+ActiveSupport::Dependencies.autoload_paths << notifications_path
 
-ActiveSupport::Dependencies.load_once_paths << events_path
-ActiveSupport::Dependencies.load_once_paths << notifications_path
+ActiveSupport::Dependencies.autoload_once_paths << events_path
+ActiveSupport::Dependencies.autoload_once_paths << notifications_path
 
 # Load notifications and events into memory
 Dir[File.join(events_path, '*')].each { |event_file| require event_file }
